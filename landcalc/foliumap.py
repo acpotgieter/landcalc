926e127450a56bb0c534d12daf3e6071a681797e
“A.C. <andreapotgieter5@gmail.com>
2025-04-30 12:24:06 -0400

    fol

---
+++ landcalc/foliumap.py
@@ -0,0 +1,12 @@
+"""Adds custom Map class that extends the folium Map class."""
+
+import os
+import folium
+import folium.plugins
+
+class Map(folium.Map):
+    """Custom Map class that extends the folium Map class."""
+
+    def __init__(self, center=(0, 0), zoom=2, **kwargs):
+        """Initialize the Map with a center and zoom level."""
+        super().__init__(location=center, zoom_start=zoom, **kwargs)
